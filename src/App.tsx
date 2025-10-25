da9691abf7a8bed6a2d9cf57bf28984a61617304
hadongz <mhadifaturrahman@gmail.com>
2025-10-25 18:16:51 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900'>Hello, world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
